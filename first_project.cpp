2983e516345375113cf07049c158d5f1a2f4533e
ank501 <ankitsyadav246@gmail.com>
2024-11-19 20:27:39 +0530

    first second and third hello program for c++ learend basic command for running the program .. we can use -o for giving name of executable file

---
+++ first_project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void print(){
+ cout<<"hello"<<endl;
+}
+
+int main (){
+   print();
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Second Program "<<endl;
+    return 0;
+}
